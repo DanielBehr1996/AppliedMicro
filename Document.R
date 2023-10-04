2b152c19b780cfb3f404d14df12bf8a5bfc472af
Daniel Behr <94843266+DanielBehr1996@users.noreply.github.com>
2023-10-04 17:17:02 +0200

    Add files via upload

---
+++ Document.R
@@ -0,0 +1,16 @@
+# Document data
+#' seasons_data Dataset
+#'
+#' A dataset containing ...
+#'
+#' @format A data frame with ...
+#' \describe{
+#'   \item{Variable1}{Description of Variable1...}
+#'   \item{Variable2}{Description of Variable2...}
+#'   ...
+#' }
+#' @source Description of where you got the data or how it was generated.
+#' @examples 
+#' data(seasons_data)
+#' head(seasons_data)
+"seasons_data"
